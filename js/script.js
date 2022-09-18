65e55ce7c40d3978baa7b4a4b158be9db6cdcaca
VictorPauk <vityalet@gmail.com>
2022-09-18 12:34:01 +0300

    header and section

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            0:{
+                items:1
+            },
+
+            1000:{
+                items:1
+            },
+        }
+    });
+});
+
+
+
+$(".navT").on("click", function(){
+    $(this).toggleClass("active");
+    $("#menu").toggleClass("open");
+    $(".content").toggleClass("shift");
+
+
+})
